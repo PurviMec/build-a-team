60fa3d2b64e62a0c32068149972d4baee0fc2602
PurviMec <princy.mecwan94@gmail.com>
2021-12-28 11:14:35 -0500

    create tests for manager and passed it

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
-const checkIfEqual = require('../lib/Manager');
+const Manager = require('../lib/Manager');
+
+test('creates Manager object', () => {
+    const manager = new Manager('employeeName', 'employeeId', 'employeeEmail', 4);
+    
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('employeeName', 'employeeId', 'employeeEmail');
+
+    expect(manager.getRole()).toEqual("Manager");
+}); 
