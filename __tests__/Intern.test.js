43b123e2b78d11f4befd49220840a52cc42eab6f
PurviMec <princy.mecwan94@gmail.com>
2021-12-28 11:01:24 -0500

    create intern tests and passed it

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
-const checkIfEqual = require('../lib/Intern');
+const Intern = require('../lib/Intern');
+
+test('create an intern obj', () => {
+    const intern = new Intern('employeeName', 'employeeId', 'employeeEmail', 'schoolName' );
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('gets intern school', () => {
+    const intern = new Intern('employeeName', 'employeeId', 'employeeEmail', 'schoolName' );
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('role of employee', () => {
+    const intern = new Intern('employeeName', 'employeeId', 'employeeEmail', 'schoolName' );
+
+    expect(intern.getRole()).toEqual('Intern');
+});
